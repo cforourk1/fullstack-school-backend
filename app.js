5e0cb036faa26b904debf8bf4e3c7e3fcbc674d6
Crystal <cforourk@umich.edu>
2026-06-25 09:25:18 -0400

    added cors package and adjusted app js

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 import morgan from "morgan";
 import express from "express";
+import cors from "cors";
+
 const app = express();
 export default app;
+
+
+
 
 // middleware
 app.use(express.json());
 app.use(morgan("dev"));
 
-// GET / to send the message "Hello Lincoln!"
-app.route("/").get((req, res) => {
-  res.send("Hello Lincoln!");
-});
+
+//cors
+
+app.use(cors({ origin: /localhost/ }));
+
+
