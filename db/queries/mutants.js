7752ecd17fc92b5094baa261ff65ea545ee21240
Crystal <cforourk@umich.edu>
2026-06-25 21:32:32 -0400

    database seeded with new info

--- db/queries/mutants.js
+++ db/queries/mutants.js
@@ -4,7 +4,7 @@ import db from "#db/client";
 export async function createMutant(name, alias, status, powerDescription, biography,  imageUrl) {
   const sql = `
   INSERT INTO mutants (name, alias, status, power_description, biography, image_url)
-  VALUES ($1, $2, $3, $4, $5, $6, $7)
+  VALUES ($1, $2, $3, $4, $5, $6)
   RETURNING *
   `;
   const { rows: [mutant] } = await db.query(sql, [name, alias, status, powerDescription, biography, imageUrl]);
--- db/queries/teams_mutants.js
+++ db/queries/teams_mutants.js
@@ -1,3 +1,16 @@
+import db from "#db/client";
+
+// add mutant to team
+export async function createTeamMutant(teamId, mutantId) {
+  const sql = `
+  INSERT INTO teams_mutants (team_id, mutant_id)
+  VALUES ($1, $2)
+  RETURNING *
+  `;
+  const { rows: [teamMutant] } = await db.query(sql, [teamId, mutantId]);
+  return teamMutant;
+}
+
 // remove mutant from team
 export async function deleteTeamMutant(teamId, mutantId) {
   const sql = `
